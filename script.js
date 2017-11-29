e4ae78703bcb47d4b64a7de40a1d48ba45c9826a
Nikaylah <nikaylah@gmail.com>
2017-11-29 14:53:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
 document.getElementsByTagName("h1")[0].style.fontSize = "80px";
